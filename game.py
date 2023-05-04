c61c6119009deb4a31e2a90578e0359b04e40270
huynh-kent <huynhkentd@gmail.com>
2023-05-04 03:33:49 -0700

    added a* notes

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 
-import copy
+from copy import deepcopy
 
 class Game:
     def __init__(self, towers, disks):
@@ -40,7 +40,7 @@ class Game:
         return valid_moves
     
     def compute_hash(self, old_tower=None, new_tower=None):
-        temp = copy.deepcopy(self.towers)
+        temp = deepcopy(self.towers)
         #print(f'temp copy {temp}')
         if old_tower:
             temp[new_tower].insert(0, temp[old_tower].pop(0))
@@ -60,31 +60,65 @@ class Game:
     
 
 if __name__ == '__main__':
-    # game = Game(3, 5)
-    # print(game)
-    # game.move(0,1)
-    # print(game)
-    # valid_moves = game.get_valid_moves()
-    # print(valid_moves)
-    game = Game(3,5)
+    game = Game(3, 5)
     print(game)
-    frontier = [copy.deepcopy(game.towers)]
-    # print(frontier)
-    # print(game.get_valid_moves())
-    while not game.is_finished():
-        valid_moves = game.get_valid_moves()
-        #print(valid_moves)
-        #print(f'frontier {frontier}')
-        while len(valid_moves) == 0:
-            #print(f'frontier before pop {frontier}')
-            #print(f'prev moves {game.prev_moves.pop()}')
-            game.towers = frontier.pop()
-            #print(f'pop frontier {frontier} game {game.towers}')
-            valid_moves = game.get_valid_moves()
-            
-        next_move = valid_moves[0]
+    game.move(0,1)
+    print(game)
+    valid_moves = game.get_valid_moves()
+    print(valid_moves)
 
-        game.move(next_move[0], next_move[1])
-        frontier.append(copy.deepcopy(game.towers))
-        print(game)
-        print(game.prev_moves)
+    # ### first valid move algorithm ###
+    # game = Game(3,5)
+    # print(game)
+    # frontier = [deepcopy(game.towers)]
+    # # print(frontier)
+    # # print(game.get_valid_moves())
+    # while not game.is_finished():
+    #     valid_moves = game.get_valid_moves()
+    #     #print(valid_moves)
+    #     #print(f'frontier {frontier}')
+    #     while len(valid_moves) == 0:
+    #         #print(f'frontier before pop {frontier}')
+    #         #print(f'prev moves {game.prev_moves.pop()}')
+    #         game.towers = frontier.pop()
+    #         #print(f'pop frontier {frontier} game {game.towers}')
+    #         valid_moves = game.get_valid_moves()
+            
+    #     next_move = valid_moves[0]
+
+    #     game.move(next_move[0], next_move[1])
+    #     frontier.append(deepcopy(game.towers))
+    #     print(game)
+    #     print(game.prev_moves)
+
+"""
+a* notes
+f(n) = g(n) + h(n)
+g is measure of cost to reach node n
+h is estimate of cost to reach goal from n
+f(x) = g(x) + h(x)
+hx = number of disks in wrong tower in state X
+gx = depth of node X in search tree
+
+initialize open list
+initialize closed list
+put start node on open list (leave f at zero)
+while open list is not empty
+    find node with least f on open list, call it q
+    pop q off the open list
+    generate q's 8 successors and set their parents to q
+    for each successor
+        if successor is the goal, stop search
+        else compute g and h for successor
+            successor.g = q.g + distance between successor and q
+            successor.h = distance from goal to successor
+            successor.f = successor.g + successor.h
+        if a node with the same position as successor is in the OPEN list
+            which has a lower f than successor, skip this successor
+        if a node with the same position as successor is in the CLOSED list
+            which has a lower f than successor, skip this successor
+        else add the node to the open list
+    end for loop
+    push q on the closed list
+end while loop
+"""

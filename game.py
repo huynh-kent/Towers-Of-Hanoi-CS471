f593f37f675346de3637579d7407886d3e9a8c0c
huynh-kent <huynhkentd@gmail.com>
2023-05-09 03:27:54 -0700

    implement a* move algorithm

--- game.py
+++ game.py
@@ -6,6 +6,9 @@ class Game:
         self.towers = [[] for tower in range(towers)]   # create towers
         self.prev_moves = []
         self.num_disks = disks
+        self.g = 0
+        self.h = 0
+        self.f = 0
         
         # add disks to first tower
         for i in range(disks):
@@ -22,7 +25,7 @@ class Game:
         else:
             self.towers[new_tower].insert(0, self.towers[old_tower].pop(0))
             move_hash = self.compute_hash(new_tower,old_tower)
-            self.prev_moves.append(move_hash)
+            if move_hash not in self.prev_moves: self.prev_moves.append(move_hash)
             return True
         
     # get valid moves from current state
@@ -53,19 +56,23 @@ class Game:
     def is_finished(self):
         if len(self.towers[-1]) == self.num_disks:
             return True
-
+        
         
     def __repr__(self):
         return str(self.towers)
     
+def get_lowest_f(open_list):
+    lowest_f = min(open_list, key=lambda state: state.f)
+    return lowest_f
+    
 
 if __name__ == '__main__':
-    game = Game(3, 5)
-    print(game)
-    game.move(0,1)
-    print(game)
-    valid_moves = game.get_valid_moves()
-    print(valid_moves)
+    # game = Game(3, 5)
+    # print(game)
+    # game.move(0,1)
+    # print(game)
+    # valid_moves = game.get_valid_moves()
+    # print(valid_moves)
 
     # ### first valid move algorithm ###
     # game = Game(3,5)
@@ -91,6 +98,63 @@ if __name__ == '__main__':
     #     print(game)
     #     print(game.prev_moves)
 
+    ### a* move algorithm ###
+    game = Game(3,3)
+    open_list = []
+    closed_list = []
+    open_list.append(game)
+    while open_list:
+        # find state with lowest f on open list
+        main_state = get_lowest_f(open_list)
+        # remove state from open list
+        #print(f'open list {open_list}')
+        open_list.remove(main_state)
+
+        # generate possible new states
+        possible_states = []
+        for valid_move in main_state.get_valid_moves():
+            new_state = deepcopy(main_state)
+            new_state.move(valid_move[0], valid_move[1])
+            possible_states.append(new_state)
+        for state in possible_states:
+            # if state is goal, stop search
+            if state.is_finished():
+                print("Finished")
+                #print(state.prev_moves)
+                break
+            else: # compute f for possible state
+                state.g = main_state.g + 1
+                #state.h = 
+                state.f = state.g + state.h
+
+            skip = False
+            # if state with same position as possible state is in open list
+            # which has lower f than possible state, skip possible state
+            for open_state in open_list:
+                if open_state.towers == state.towers and open_state.f < state.f:
+                    skip = True
+            # if state with same position as possible state is in closed list
+            # which has lower f than possible state, skip possible state
+            for closed_state in closed_list:
+                if closed_state.towers == state.towers and closed_state.f < state.f:
+                    skip = True
+            
+
+            
+            if skip: continue
+            # add possible state to open list
+            open_list.append(state)
+        # push main state on closed list
+        closed_list.append(main_state)
+        #print(f'closed list {closed_list}')
+                
+
+        
+
+
+
+
+
 """
 a* notes
 f(n) = g(n) + h(n)
@@ -100,25 +164,25 @@ f(x) = g(x) + h(x)
 hx = number of disks in wrong tower in state X
 gx = depth of node X in search tree
 
-initialize open list
-initialize closed list
-put start node on open list (leave f at zero)
-while open list is not empty
-    find node with least f on open list, call it q
-    pop q off the open list
-    generate q's 8 successors and set their parents to q
-    for each successor
-        if successor is the goal, stop search
-        else compute g and h for successor
-            successor.g = q.g + distance between successor and q
-            successor.h = distance from goal to successor
-            successor.f = successor.g + successor.h
-        if a node with the same position as successor is in the OPEN list
-            which has a lower f than successor, skip this successor
-        if a node with the same position as successor is in the CLOSED list
-            which has a lower f than successor, skip this successor
-        else add the node to the open list
-    end for loop
-    push q on the closed list
-end while loop
+# initialize open list
+# initialize closed list
+# put start node on open list (leave f at zero)
+# while open list is not empty
+#     find node with least f on open list, call it q
+#     pop q off the open list
+#     generate q's 8 successors and set their parents to q
+#     for each successor
+#         if successor is the goal, stop search
+#         else compute g and h for successor
+#             successor.g = q.g + distance between successor and q
+#             successor.h = distance from goal to successor
+#             successor.f = successor.g + successor.h
+#         if a node with the same position as successor is in the OPEN list
+#             which has a lower f than successor, skip this successor
+#         if a node with the same position as successor is in the CLOSED list
+#             which has a lower f than successor, skip this successor
+#         else add the node to the open list
+#     end for loop
+#     push q on the closed list
+# end while loop
 """
